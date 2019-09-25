568e6753fda32257d7fee497db6593eb67d47e72
Victor <victor_polyukh@ukt.net>
2019-09-25 22:06:50 +0300

    Add ip

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+  require "rb.php";
+  R::setup( 'mysql:host=localhost;dbname=ucounter', 'root', '' );
+
+  $ip = $_SERVER['REMOTE_ADDR'];
+  $online = R::findOne('online', 'ip = ?', array($ip));
+
+  if($online){
+    // Update
+
+  }else{
+    // Create
+    $online = R::dispense('online');
+    $online->lastvisit = time();
+    $online->ip = $ip;
+    R::store($online);
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
